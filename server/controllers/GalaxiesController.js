53f2696a931ec659c0d927585ef46314e4892e87
Joseph Bowers <jsphbowers@gmail.com>
2023-03-22 16:07:04 -0600

    Finished Galaxies

--- server/controllers/GalaxiesController.js
+++ server/controllers/GalaxiesController.js
@@ -6,6 +6,8 @@ export class GalaxiesController extends BaseController {
     super('api/Galaxies')
     this.router
       .get('', this.getGalaxies)
+      .get('/:galaxyId', this.getGalaxyById)
+      .post('', this.createGalaxy)
   }
   async getGalaxies(req, res, next) {
     try {
@@ -14,6 +16,25 @@ export class GalaxiesController extends BaseController {
       return res.send(galaxies)
     } catch (error) {
       next(error)
+    }
+  }
+  async getGalaxyById(req, res, next) {
+    try {
+      const galaxyId = req.params.galaxyId
+      const galaxy = await galaxiesService.getGalaxyById(galaxyId)
+      return res.send(galaxy)
+    } catch (error) {
+      next.error(error)
+    }
+
+  }
+  async createGalaxy(req, res, next) {
+    try {
+      const galaxyData = req.body
+      const newGalaxy = await galaxiesService.createGalaxy(galaxyData)
+      return res.send(newGalaxy)
+    } catch (error) {
+      next.error(error)
     }
   }
 }
--- server/services/GalaxiesService.js
+++ server/services/GalaxiesService.js
@@ -1,9 +1,21 @@
 import { dbContext } from "../db/DbContext.js"
+import { BadRequest } from "../utils/Errors.js"
 
 class GalaxiesService {
   async getGalaxies(query) {
     const galaxies = await dbContext.Galaxies.find(query)
     return galaxies
+  }
+  async getGalaxyById(galaxyId) {
+    const galaxy = await dbContext.Galaxies.findById(galaxyId)
+    if (!galaxy) {
+      throw new BadRequest('Not a single galaxy by that ID you gave me, try again')
+    }
+    return galaxy
+  }
+  async createGalaxy(galaxyData) {
+    const newGalaxy = await dbContext.Galaxies.create(galaxyData)
+    return newGalaxy
   }
 
 }
